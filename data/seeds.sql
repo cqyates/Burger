0b7d600b93da99a0db5c7841e928f0c2517131b5
Corey <cqyates@gmail.com>
2019-05-26 15:06:26 -0400

    added mysql connection

---
+++ data/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Kahuna Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Burger", FALSE);
